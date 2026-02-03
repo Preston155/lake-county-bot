85d493c4694facdd54b1bc1d85a4e3a4536f7dc7
Preston155 <prestonosborne8@gmail.com>
2026-02-02 22:08:16 -0500

    Add initial data structure in data.js

---
+++ data.js
@@ -0,0 +1,11 @@
+{
+  "counting": {
+    "channelId": "1466631881440100493",
+    "currentCount": 0,
+    "lastCounterId": null
+  },
+  "warnings": {
+    "976524719752884234": [],
+    "1428885916549644369": []
+  }
+}
